71bb3914713b6fb6c435bef65d173790053c23e5
Emily Cross <emilycross@t89b0.wpa.dal.ca>
2016-11-03 16:19:54 -0300

    Initial Commit

---
+++ JognJam/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  JognJam
+//
+//  Created by Emily Cross on 2016-11-03.
+//  Copyright © 2016 Team ACED. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
